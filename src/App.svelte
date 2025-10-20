430fad0b967345c24b2dafc049d99228e7b1dd4f
Muhammad Afif Ramadhan <ramadhanafif@gmail.com>
2025-10-20 08:05:17 +0200

    rewrite to vite svelte

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main
+  class="flex min-h-screen flex-col items-center bg-gray-100 justify-between"
+>
+  <header class="w-full px-8 py-8">
+    <h1 class="text-3xl font-bold">
+      <span class="text-indigo-800 transition-all">ForSyDe</span> SDF Graph Demo
+    </h1>
+    <h2 class="text-slate-400">
+      using <a
+        href="https://js.cytoscape.org/"
+        class="hover:underline hover:text-blue-300 transition-all decoration-blue-400"
+        >Cytoscape.js</a
+      > library
+    </h2>
+  </header>
+
+  <div id="cy-canvas"></div>
+</main>
